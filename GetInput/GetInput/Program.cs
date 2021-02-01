a5ed24992ad783d8f89c47b725959d765d6633f0
masfikurfahim <78055576+masfikurfahim@users.noreply.github.com>
2021-02-01 10:32:28 +0600

    GetInput

---
+++ GetInput/GetInput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello, {0} the current time is {1}", userName, System.DateTime.Now.TimeOfDay);
+
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey();
+        }
+    }
+}
