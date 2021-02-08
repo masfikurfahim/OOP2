f985f2336c9338f20a6ce4d4c0f057efb15e8b73
masfikurfahim <78055576+masfikurfahim@users.noreply.github.com>
2021-02-08 10:38:48 +0600

    Lab 3

---
+++ Lab 3 Solution 4/Lab 3 Solution 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab_3_Solution_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int even = 0;
+            int odd = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    even = even + i;
+                }
+                else
+                {
+                    odd = odd + i;
+                }
+            }
+            Console.WriteLine(even);
+            Console.WriteLine(odd);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab 3 Solution 8/Lab 3 Solution 8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_3_Solution_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Physics = 80f;
+            float Chemistry = 85f;
+            float Biology = 75f;
+            float Math = 70f;
+            float Computer = 100f;
+            float result = (Physics + Chemistry + Biology + Math + Computer)/5;
+            Console.WriteLine("Total Marks " + result);
+            if (result >= 90) { Console.WriteLine("Grade A+"); }
+            else if (result >= 85 && result < 90) { Console.WriteLine("Grade A"); }
+            else if (result >= 80 && result < 85) { Console.WriteLine("Grade B+"); }
+            else if (result >= 75 && result < 80) { Console.WriteLine("Grade B"); }
+            else if (result >= 50 && result < 75) { Console.WriteLine("Grade C+"); }
+            else { Console.WriteLine("Grade F"); }
+        }
+    }
+}
