8416c7a4d6d86214fb2800d9e107ccf2803e92b3
masfikurfahim <78055576+masfikurfahim@users.noreply.github.com>
2021-03-01 11:17:29 +0600

    Association

---
+++ Student Data/Student Data/Department.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student_Data
+{
+    class Department
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        Student[] students;
+        int studentCount;
+
+        public Department(string name)
+        {
+            this.name = name;
+            students = new Student[50];
+        }
+
+        public void AddStudent(params Student[] stu)
+        {
+            foreach (Student s in stu)
+            {
+                students[studentCount++] = s;
+                s.Dept = this;
+            }
+        }
+
+        public void PrintStudent()
+        {
+            foreach (Student s in students)
+                if (s != null)
+                    s.ShowInfo();
+        }
+    }
+}
+
---
+++ Student Data/Student Data/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Student_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department d1 = new Department("Computer Science");
+            Department d2 = new Department("Electrical Engineering");
+
+            Student s1 = new Student("Tanvir", "13245", 2.50);
+            Student s2 = new Student("Sabbir", "13246", 3.50);
+            Student s3 = new Student("Rahim", "132447", 3.00);
+            Student s4 = new Student("Sakib", "132448", 4.00);
+
+            d1.AddStudent(s1, s2);
+            d2.AddStudent(s3);
+            d2.AddStudent(s4);
+
+            d1.PrintStudent();
+            d2.PrintStudent();
+
+            Console.ReadLine();
+        }
+    }
+}
